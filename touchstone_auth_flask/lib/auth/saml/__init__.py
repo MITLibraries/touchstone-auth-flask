a98563f16286c09258e34d0ef96a479d43c31083
Graham Hukill <ghukill@gmail.com>
2024-06-27 14:40:29 -0400

    Set some IdP and SP configurations dynamically
    Why these changes are being introduced:
    
    Information like the IdP x509 certificate and SSO URL can be retrieved
    from the IdP metadata.  Retrieving this dynamically makes it more flexible to
    IdP changes, and reduces the need to store sensitive information even in AWS
    params.
    
    We must also provide the SP certificate and key.  For local development
    formerly this required generating a self-signed certificate and setting that as
    environment variables.  It would be handy if when locally developing
    this was handled automatically.
    
    How this addresses that need:
    * Retrieves IdP SSO URL and x509 certificate once and saves to Flask app config
    * Dynamically creates self-signed certificate for local development for SP
    
    Side effects of this change:
    * IdP SSO URL and certificate will not be required as AWS parameters
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-1008

--- touchstone_auth_flask/lib/auth/saml/__init__.py
+++ touchstone_auth_flask/lib/auth/saml/__init__.py
@@ -1,8 +1,17 @@
+# ruff: noqa: S603
+
+import logging
+import os
+import subprocess
+import tempfile
 from urllib.parse import urlparse
 from xml.dom.minidom import Document
 
 from flask import Request, current_app
 from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from onelogin.saml2.idp_metadata_parser import OneLogin_Saml2_IdPMetadataParser
+
+logger = logging.getLogger(__name__)
 
 DEFAULT_SAML_SETTINGS: dict = {
     "strict": True,
@@ -42,30 +51,97 @@ def get_saml_auth_obj(request: Request) -> OneLogin_Saml2_Auth:
 
 
 def load_saml_settings() -> dict:
-    """Load SAML settings for use in metadata generation and communication with the IdP
+    """Load SAML settings for all IdP/SP interactions.
 
-    This method overrides or adds values to what exists in the saml/settings.json file
+    This function overrides or adds values to DEFAULT_SAML_SETTINGS defined above.
     """
     saml_settings = DEFAULT_SAML_SETTINGS
 
-    saml_settings["debug"] = True
+    set_idp_sso_url_and_cert()
+    set_development_sp_key_and_cert()
 
+    saml_settings["debug"] = True
     saml_settings["sp"]["entityId"] = current_app.config["SP_ENTITY_ID"]
     saml_settings["sp"]["assertionConsumerService"]["url"] = current_app.config[
         "SP_ACS_URL"
     ]
     saml_settings["sp"]["x509cert"] = current_app.config["SP_CERT"]
     saml_settings["sp"]["privateKey"] = current_app.config["SP_KEY"]
-
     saml_settings["idp"]["entityId"] = current_app.config["IDP_ENTITY_ID"]
     saml_settings["idp"]["singleSignOnService"]["url"] = current_app.config["IDP_SSO_URL"]
     saml_settings["idp"]["x509cert"] = current_app.config["IDP_CERT"]
-
     saml_settings["security"]["wantAssertionsEncrypted"] = current_app.config.get(
         "SP_SECURITY_ASSERTIONS_ENCRYPTED", False
     )
 
     return saml_settings
+
+
+def set_idp_sso_url_and_cert() -> None:
+    """Set IDP SSO URL and certificate from IdP metadata."""
+    if (
+        current_app.config["IDP_SSO_URL"] is None
+        or current_app.config["IDP_CERT"] is None
+    ):
+        idp_metadata = retrieve_and_parse_idp_metadata()
+        current_app.config["IDP_SSO_URL"] = idp_metadata["singleSignOnService"]["url"]
+        current_app.config["IDP_CERT"] = idp_metadata["x509cert"]
+
+
+def set_development_sp_key_and_cert() -> None:
+    """Set SP key and certificate by generating a locally signed certificate.
+
+    NOTE: this should be used only for development purposes and will throw an exception
+     otherwise.
+    """
+    if current_app.config["SP_KEY"] is None or current_app.config["SP_CERT"] is None:
+
+        if os.getenv("FLASK_ENV") not in ["development", "testing"]:
+            message = "Environment variables SP_KEY and SP_CERT must be set."
+            raise RuntimeError(message)
+
+        current_app.config["SP_KEY"], current_app.config["SP_CERT"] = (
+            generate_development_sp_x509_cert_and_key()
+        )
+
+
+def retrieve_and_parse_idp_metadata() -> dict:
+    """Retrieve and parse metadata from remote IdP."""
+    idp_entity_url = current_app.config["IDP_ENTITY_ID"]
+    return OneLogin_Saml2_IdPMetadataParser.parse_remote(idp_entity_url)["idp"]
+
+
+def generate_development_sp_x509_cert_and_key() -> tuple[str, str]:
+    """Generate a locally signed certificate and return values."""
+    with tempfile.NamedTemporaryFile(
+        suffix=".pem"
+    ) as key_file, tempfile.NamedTemporaryFile(suffix=".pem") as cert_file:
+        openssl_command = [
+            "openssl",
+            "req",
+            "-new",
+            "-x509",
+            "-nodes",
+            "-newkey",
+            "rsa:2048",
+            "-keyout",
+            key_file.name,
+            "-days",
+            "3650",
+            "-out",
+            cert_file.name,
+            "-subj",
+            "/C=US/ST=Massachusetts/L=Cambridge/O=MIT/OU=MIT Libraries/CN=localhost",
+        ]
+        subprocess.run(
+            openssl_command,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+            check=True,
+        )
+        local_key = key_file.read().decode()
+        local_cert = cert_file.read().decode()
+    return local_key, local_cert
 
 
 def prepare_flask_request(request: Request) -> dict:
